e2d6db7a489bec653f34a172d8737e98dd04e260
Alexander Zielke <vulture@clutterbox.de>
2019-07-13 12:18:25 +0200

    try fixing CI

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_certificate_file_exists(host):
+    f = host.file('/etc/dehydrated/certs/example.com/cert.pem')
 
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
