edebb92910fa169e441b026cb19b8231157cc897
Alexander Zielke <vulture@clutterbox.de>
2019-07-13 12:28:05 +0200

    check if all certificate files exist

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,6 +7,12 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_certificate_file_exists(host):
-    f = host.file('/etc/dehydrated/certs/example.com/cert.pem')
+    cert = host.file('/etc/dehydrated/certs/example.com/cert.pem')
+    chain = host.file('/etc/dehydrated/certs/example.com/chain.pem')
+    fullchain = host.file('/etc/dehydrated/certs/example.com/fullchain.pem')
+    privkey = host.file('/etc/dehydrated/certs/example.com/privkey.pem')
 
-    assert f.exists
+    assert cert.exists
+    assert chain.exists
+    assert fullchain.exists
+    assert privkey.exists
